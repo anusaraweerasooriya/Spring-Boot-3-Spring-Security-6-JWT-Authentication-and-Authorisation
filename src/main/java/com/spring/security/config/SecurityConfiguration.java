005e6e78a18c975adc44a2cd9dad86ad364589a6
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-08-03 15:53:47 +0530

    final commit

--- src/main/java/com/spring/security/config/SecurityConfiguration.java
+++ src/main/java/com/spring/security/config/SecurityConfiguration.java
@@ -20,13 +20,13 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
